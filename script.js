55e213df34c3c64f842b409e9f99db6c50dd36d8
Bikram <bikram@Bikrams-MacBook-Pro.local>
2016-04-14 19:50:00 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,4 @@
+angular.module('myApp', [ui.router])
+                                    .controller('myCtrl', function($scope){
+                                      $scope.message='hi';
+                                    });
